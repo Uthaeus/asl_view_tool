6130546b75bcd1f0e3faa9ead446e3c58c199a68
Uthaeus <Uth@github.com>
2017-12-29 22:23:25 -0700

    ASL tool initial commit

---
+++ lib/asl_view_tool.rb
@@ -0,0 +1,6 @@
+require "asl_view_tool/version"
+require "asl_view_tool/renderer"
+
+module AslViewTool
+  # Your code goes here...
+end
---
+++ spec/asl_view_tool_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe AslViewTool do
+  it "has a version number" do
+    expect(AslViewTool::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
